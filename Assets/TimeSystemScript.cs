57e4bb3cf4c17931a7e19ffcee40db661218a6e2
Giocumber <giosalceda@gmail.com>
2024-10-18 01:21:46 +0800

    Added Timer Countdown

---
+++ Assets/TimeSystemScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeSystemScript : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;  // Assign your TextMeshPro UI element in the Inspector
+    public float timeRemaining = 60f;  // The starting time in seconds
+    private bool isCountingDown = true;
+
+    public GameObject timesUpText;
+
+    void Update()
+    {
+        if (isCountingDown && timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;  // Reduce time based on how much time has passed since the last frame
+            UpdateTimerUI(timeRemaining);
+        }
+        else if (timeRemaining <= 0)
+        {
+            timeRemaining = 0;  // Make sure it doesn't go below 0
+            timerText.text = ("00:00");
+            isCountingDown = false;  // Stop the countdown
+            TimerEnded();
+        }
+    }
+
+    void UpdateTimerUI(float currentTime)
+    {
+        // Format the time in minutes and seconds (00:00 format)
+        int minutes = Mathf.FloorToInt(currentTime / 60f);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+
+        if (minutes > 0 || seconds > 0)
+            timerText.text = string.Format("{00:00}:{01:00}", minutes, seconds);
+    }
+
+    void TimerEnded()
+    {
+        timesUpText.SetActive(true);
+        Invoke("DisableTimesUpText", 4f);
+    }
+
+    void DisableTimesUpText()
+    {
+        timesUpText.SetActive(false);
+    }
+}
